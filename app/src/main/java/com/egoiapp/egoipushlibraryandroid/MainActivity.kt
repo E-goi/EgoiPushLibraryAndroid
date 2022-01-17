0889177739bfbfb8b1a6351f7096c6c3622cfc97
Diogo Pereira <dpereira@e-goi.com>
2022-01-17 16:17:14 +0000

    improv(BB-19358): remove observers to register token worker

--- app/src/main/java/com/egoiapp/egoipushlibraryandroid/MainActivity.kt
+++ app/src/main/java/com/egoiapp/egoipushlibraryandroid/MainActivity.kt
@@ -60,12 +60,16 @@ class MainActivity : EgoiPushActivity() {
 
             if(request !== null) {
                 WorkManager.getInstance(this).getWorkInfoByIdLiveData(request.id)
-                    .observe(this, Observer { workInfo ->
+                    .observe(this, { workInfo ->
                         if (workInfo != null && workInfo.state.isFinished) {
                             // validate if state is equal to SUCCEEDED when token registered
+                            if(workInfo.state == WorkInfo.State.FAILED) {
+                                Log.d("TOKEN", "failed");
+                            }
                             if(workInfo.state == WorkInfo.State.SUCCEEDED) {
                                 Log.d("TOKEN", "success");
                             }
+                            WorkManager.getInstance(this).getWorkInfoByIdLiveData(request.id).removeObservers(this);
                         }
                     })
             }
