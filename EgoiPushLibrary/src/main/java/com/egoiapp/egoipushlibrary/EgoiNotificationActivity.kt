e0ba9a1366b3349998a20d5613d2b46808eb4627
Diogo Pereira <dpereira@e-goi.com>
2022-09-15 10:06:20 +0100

    improv(BB-21110): call dialog when app are closed

--- EgoiPushLibrary/src/main/java/com/egoiapp/egoipushlibrary/EgoiNotificationActivity.kt
+++ EgoiPushLibrary/src/main/java/com/egoiapp/egoipushlibrary/EgoiNotificationActivity.kt
@@ -1,9 +1,15 @@
 package com.egoiapp.egoipushlibrary
 
+import android.app.AlertDialog
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
 import com.egoiapp.egoipushlibrary.receivers.NotificationEventReceiver
+import com.egoiapp.egoipushlibrary.structures.EgoiNotification
 
 class EgoiNotificationActivity : AppCompatActivity() {
     private var intentProcessed: Boolean = false
@@ -16,7 +22,6 @@ class EgoiNotificationActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
         EgoiPushLibrary.getInstance(this).location.stopService()
 
         if (intentProcessed) {
@@ -28,20 +33,124 @@ class EgoiNotificationActivity : AppCompatActivity() {
 
     private fun processIntent() {
         intentProcessed = true
-        if (intent.action == NotificationEventReceiver.LOCATION_NOTIFICATION_OPEN) {
+        if (intent.action == LOCATION_NOTIFICATION_OPEN) {
             finishActivity()
-        } else if (intent.action in arrayOf(applicationContext.packageName + NotificationEventReceiver.NOTIFICATION_OPEN, applicationContext.packageName + NotificationEventReceiver.NOTIFICATION_ACTION_VIEW)) {
-            var intentReceiver = Intent(applicationContext, NotificationEventReceiver::class.java)
-            intentReceiver.action = intent.action
-            intent.extras?.let { intentReceiver.putExtras(it) }
-            sendBroadcast(intentReceiver)
+        } else if (intent.action in arrayOf(NOTIFICATION_OPEN, NOTIFICATION_ACTION_VIEW)) {
 
-            if (!EgoiPushLibrary.IS_INITIALIZED) {
-                var intentPackage = packageManager.getLaunchIntentForPackage(packageName)!!
-                startActivity(intentPackage)
+            val egoiNotification = EgoiNotification(
+                title = intent.extras?.getString("title") ?: "",
+                body = intent.extras?.getString("body") ?: "",
+                actionType = intent.extras?.getString("actionType") ?: "",
+                actionText = intent.extras?.getString("actionText") ?: "",
+                actionUrl = intent.extras?.getString("actionUrl") ?: "",
+                actionTextCancel = intent.extras?.getString("actionTextCancel") ?: "",
+                apiKey = intent.extras?.getString("apiKey") ?: "",
+                appId = intent.extras?.getString("appId") ?: "",
+                contactId = intent.extras?.getString("contactId") ?: "",
+                messageHash = intent.extras?.getString("messageHash") ?: "",
+                deviceId = intent.extras?.getInt("deviceId", 0) ?: 0,
+                messageId = intent.extras?.getInt("messageId", 0) ?: 0
+            )
+
+            if (intent.action == NOTIFICATION_OPEN) {
+                if (egoiNotification.actionType != "" && egoiNotification.actionText != "" && egoiNotification.actionUrl != "" && egoiNotification.actionTextCancel != "") {
+                    if (!EgoiPushLibrary.IS_INITIALIZED) {
+                        intentProcessed = false
+                        var intentBroadcast = Intent(applicationContext, NotificationEventReceiver::class.java)
+                        intentBroadcast.action = applicationContext.packageName + NotificationEventReceiver.NOTIFICATION_OPEN
+                        intent.extras?.let { intentBroadcast.putExtras(it) }
+                        sendBroadcast(intentBroadcast)
+                        finish()
+                    } else {
+                        if (EgoiPushLibrary.getInstance(applicationContext).dialogCallback != null) {
+                            EgoiPushLibrary.getInstance(applicationContext).dialogCallback?.let {
+                                it(egoiNotification)
+                            }
+                            finishActivity()
+                        } else {
+                            fireDialog(egoiNotification)
+                        }
+                    }
+                } else {
+                    EgoiPushLibrary.getInstance(applicationContext)
+                        .registerEvent(EgoiPushLibrary.OPEN_EVENT, egoiNotification)
+                    finishActivity()
+                }
+            } else if (intent.action == NOTIFICATION_ACTION_VIEW) {
+                EgoiPushLibrary.getInstance(applicationContext)
+                    .registerEvent(EgoiPushLibrary.OPEN_EVENT, egoiNotification)
+
+                if (egoiNotification.actionType == "deeplink") {
+                    EgoiPushLibrary.getInstance(applicationContext).deepLinkCallback?.let {
+                        it(egoiNotification)
+                    }
+                    finishActivity()
+                } else {
+                    val uriIntent =
+                        Intent(Intent.ACTION_VIEW, Uri.parse(egoiNotification.actionUrl))
+                    startActivity(uriIntent)
+                }
+
+                val notificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                notificationManager.cancel(egoiNotification.messageId)
             }
-            finish()
         }
+    }
+
+    private fun fireDialog(egoiNotification: EgoiNotification) {
+        val builder: AlertDialog.Builder =
+            AlertDialog.Builder(this)
+
+        builder.setTitle(egoiNotification.title)
+        builder.setMessage(egoiNotification.body)
+
+        if (
+            egoiNotification.actionType != "" &&
+            egoiNotification.actionText != "" &&
+            egoiNotification.actionUrl != "" &&
+            egoiNotification.actionTextCancel != ""
+        ) {
+            builder.setPositiveButton(egoiNotification.actionText)
+            { _, _ ->
+                EgoiPushLibrary.getInstance(applicationContext)
+                    .registerEvent(EgoiPushLibrary.OPEN_EVENT, egoiNotification)
+
+                if (egoiNotification.actionType == "deeplink") {
+                    EgoiPushLibrary.getInstance(applicationContext).deepLinkCallback?.let {
+                        it(egoiNotification)
+                    }
+                    finishActivity()
+                } else {
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse(egoiNotification.actionUrl)
+                        )
+                    )
+                }
+            }
+
+            builder.setNegativeButton(egoiNotification.actionTextCancel)
+            { _, _ ->
+                EgoiPushLibrary.getInstance(applicationContext)
+                    .registerEvent(EgoiPushLibrary.CANCEL_EVENT, egoiNotification)
+                finishActivity()
+            }
+
+            builder.setOnCancelListener {
+                finish()
+            }
+        }
+
+        val mainHandler = Handler(applicationContext.mainLooper)
+
+        val runnable = Runnable {
+            builder.show()
+        }
+
+        mainHandler.post(runnable)
     }
 
     private fun finishActivity() {
@@ -49,6 +158,14 @@ class EgoiNotificationActivity : AppCompatActivity() {
             val intent: Intent = packageManager.getLaunchIntentForPackage(packageName)!!
             startActivity(intent)
         }
+
         finish()
+    }
+
+    companion object {
+        const val TAG = "ACTIVITY"
+        const val NOTIFICATION_OPEN: String = "EGOI_NOTIFICATION_OPEN"
+        const val NOTIFICATION_ACTION_VIEW: String = "EGOI_NOTIFICATION_ACTION_VIEW"
+        const val LOCATION_NOTIFICATION_OPEN: String = "EGOI_LOCATION_NOTIFICATION_OPEN"
     }
 }
--- EgoiPushLibrary/src/main/java/com/egoiapp/egoipushlibrary/receivers/NotificationEventReceiver.kt
+++ EgoiPushLibrary/src/main/java/com/egoiapp/egoipushlibrary/receivers/NotificationEventReceiver.kt
@@ -1,17 +1,13 @@
 package com.egoiapp.egoipushlibrary.receivers
 
-import android.app.AlertDialog
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import android.os.Handler
-import android.os.Looper
+import com.egoiapp.egoipushlibrary.EgoiNotificationActivity
 import com.egoiapp.egoipushlibrary.EgoiPushLibrary
 import com.egoiapp.egoipushlibrary.structures.EgoiNotification
 import kotlin.concurrent.thread
-
 
 /**
  * Receiver responsible for handling the clicks on notifications triggered by a geofence
@@ -25,7 +21,7 @@ class NotificationEventReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (
             context != null && intent != null &&
-            intent.action in arrayOf(context.applicationContext.packageName + NOTIFICATION_OPEN, context.applicationContext.packageName + NOTIFICATION_ACTION_VIEW, context.applicationContext.packageName + NOTIFICATION_CLOSE)
+            (intent.action == context.applicationContext.packageName + NOTIFICATION_OPEN || intent.action == context.applicationContext.packageName + NOTIFICATION_CLOSE)
         ) {
             val extras = intent.extras
 
@@ -45,121 +41,45 @@ class NotificationEventReceiver : BroadcastReceiver() {
             )
 
             if (!EgoiPushLibrary.IS_INITIALIZED) {
-                var intentPackage = context.packageManager.getLaunchIntentForPackage(context.packageName)!!
-                context.startActivity(intentPackage)
+                val intentActivity = context.packageManager.getLaunchIntentForPackage(context.packageName)!!
+                context.startActivity(intentActivity)
             }
 
-            thread {
-                while (!EgoiPushLibrary.IS_INITIALIZED) {
-                    Thread.sleep(500)
-                }
+            if (intent.action == context.applicationContext.packageName + NOTIFICATION_OPEN) {
+                thread {
+                    while (!EgoiPushLibrary.IS_INITIALIZED) {
+                        Thread.sleep(500)
+                    }
 
-                if (intent.action == context.packageName + NOTIFICATION_OPEN) {
-                    if (egoiNotification.actionType != "" && egoiNotification.actionText != "" && egoiNotification.actionUrl != "" && egoiNotification.actionTextCancel != "") {
-
-                        if (EgoiPushLibrary.getInstance(context).dialogCallback != null) {
-                            EgoiPushLibrary.getInstance(context).dialogCallback?.let {
-                                it(egoiNotification)
-                            }
-                        } else {
-                            fireDialog(EgoiPushLibrary.getInstance(context).activityContext, egoiNotification)
+                    if (EgoiPushLibrary.getInstance(context.applicationContext).dialogCallback != null) {
+                        EgoiPushLibrary.getInstance(context.applicationContext).dialogCallback?.let {
+                            it(egoiNotification)
                         }
                     } else {
-                        EgoiPushLibrary.getInstance(context)
-                            .registerEvent(EgoiPushLibrary.OPEN_EVENT, egoiNotification)
-                        val notificationManager =
-                            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-                        notificationManager.cancel(egoiNotification.messageId)
+                        val intentActivity = Intent(context, EgoiNotificationActivity::class.java)
+                        intentActivity.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        intentActivity.action = EgoiNotificationActivity.NOTIFICATION_OPEN
+                        extras?.let { intentActivity.putExtras(it) }
+                        context.startActivity(intentActivity)
                     }
-                } else if (intent.action == context.packageName + NOTIFICATION_ACTION_VIEW) {
-                    if (egoiNotification.actionType == "deeplink") {
-                        EgoiPushLibrary.getInstance(context).deepLinkCallback?.let {
-                            it(egoiNotification)
-                        }
-                    } else if (egoiNotification.actionType == "url") {
-                        val intentUrl = Intent(
-                            Intent.ACTION_VIEW,
-                            Uri.parse(egoiNotification.actionUrl)
-                        )
-                        intentUrl.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        context.startActivity(intentUrl)
-                    }
-
-                    val notificationManager =
-                        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-                    notificationManager.cancel(egoiNotification.messageId)
                 }
-                else if (intent.action == context.applicationContext.packageName+NOTIFICATION_CLOSE) {
-                    EgoiPushLibrary.getInstance(context.applicationContext)
-                        .registerEvent(EgoiPushLibrary.CANCEL_EVENT, egoiNotification)
+            }
 
-                    val notificationManager =
-                        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (intent.action == context.applicationContext.packageName + NOTIFICATION_CLOSE) {
+                EgoiPushLibrary.getInstance(context.applicationContext)
+                    .registerEvent(EgoiPushLibrary.CANCEL_EVENT, egoiNotification)
 
-                    notificationManager.cancel(egoiNotification.messageId)
-                }
+                val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                notificationManager.cancel(egoiNotification.messageId)
             }
         }
     }
-
-    private fun fireDialog(mContext: Context, egoiNotification: EgoiNotification) {
-        val builder: AlertDialog.Builder =
-            AlertDialog.Builder(mContext)
-
-        with(builder) {
-            setTitle(egoiNotification.title)
-            setMessage(egoiNotification.body)
-
-            if (
-                egoiNotification.actionType != "" &&
-                egoiNotification.actionText != "" &&
-                egoiNotification.actionUrl != "" &&
-                egoiNotification.actionTextCancel != ""
-            ) {
-                setPositiveButton(egoiNotification.actionText)
-                { _, _ ->
-                    EgoiPushLibrary.getInstance(context)
-                        .registerEvent(EgoiPushLibrary.OPEN_EVENT, egoiNotification)
-                    if (egoiNotification.actionType == "deeplink") {
-                        EgoiPushLibrary.getInstance(context).deepLinkCallback?.let {
-                            it(egoiNotification)
-                        }
-                        /*finishActivity()*/
-                    } else if(egoiNotification.actionType == "url"){
-                        context.startActivity(
-                            Intent(
-                                Intent.ACTION_VIEW,
-                                Uri.parse(egoiNotification.actionUrl)
-                            )
-                        )
-                    }
-                }
-
-                setNegativeButton(egoiNotification.actionTextCancel)
-                { _, _ ->
-                    EgoiPushLibrary.getInstance(context)
-                        .registerEvent(EgoiPushLibrary.CANCEL_EVENT, egoiNotification)
-                }
-            }
-
-            val mainHandler = Handler(Looper.getMainLooper())
-
-            val runnable = Runnable {
-                val dialog = create()
-                dialog.show()
-            }
-
-            mainHandler.post(runnable)
-        }
-    }
-
 
     companion object {
-        const val NOTIFICATION_CLOSE: String = ".EGOI_NOTIFICATION_CLOSE"
         const val NOTIFICATION_OPEN: String = ".EGOI_NOTIFICATION_OPEN"
-        const val NOTIFICATION_ACTION_VIEW: String = ".EGOI_NOTIFICATION_ACTION_VIEW"
-        const val LOCATION_NOTIFICATION_OPEN: String = "EGOI_LOCATION_NOTIFICATION_OPEN"
+        const val NOTIFICATION_CLOSE: String = ".EGOI_NOTIFICATION_CLOSE"
     }
+
 }
